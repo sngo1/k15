7b4dbb04bf86030146132a2611cd6da737aae62e
sngo1 <sngo1@stuy.edu>
2018-04-25 21:53:34 -0400

    Finished assignment

--- comprehensions.py
+++ comprehensions.py
@@ -1,4 +1,7 @@
 # Samantha Ngo
+# Softdev -- pd7
+# k15
+# 2018-04-25
 # List Comprehensions
 
 # First Task: Check if password has upper and lower case letters and at least
@@ -38,14 +41,15 @@ Strength Strategies:
 - Dictionary Attack -> Passphrases
 - Character Attack -> Increase # of characters
 
-Min: 8 letters(>0 Upper & >0 Lower) + 1 non-alphanumeric + 1 number
-Medium: 12 letters + 2 non-alpha + 1 number
-Strong: 15 letters + 4 non-alpha + 2 numbers 
+Min: 8 letters(>0 Upper & >0 Lower) + 1 non-alphanumeric + 1 number = >8 characters
+Medium: 12 letters + 2 non-alpha + 1 number = >15 characters
+Strong: 15 letters + 4 non-alpha + 2 numbers = >21 characters
 '''
 
 nonAlphas = ".?!&#,;:-_*"
 
 def strength(password):
+    print "----------------------------------------------------------------"
     uppers = len([1 for x in password if x in UC_LETTERS])
     print "UPPERS: ", uppers
     lowers = len([1 for x in password if x in LC_LETTERS])
@@ -56,13 +60,24 @@ def strength(password):
     print "NONS: ", nons
 
     # Top-Down Criterion Check
-    if (uppers + lowers >= 15 and 
-        print "Pass: ", password, " meets criteria."
-        return True;
+    if (uppers + lowers >= 15 and nons >= 4 and numbers >= 2):
+        print "Pass: ", password, " meets criteria and is STRONG."
+        return "STRONG";
+    elif (uppers + lowers >= 12 and nons >= 2 and numbers >= 1):
+        print "Pass: ", password, " meets criteria and is MEDIUM."
+        return "MEDIUM";
+    elif (uppers + lowers >= 8 and nons >= 1 and numbers >= 1):
+        print "Pass: ", password, " meets criteria and is WEAK."
+        return "WEAK";
     else:
         print "Pass: ", password, " DOES NOT meet criteria."
-        return False
+        return "FAIL"
 
 strength("HelloWorld")
+strength("HelloWorld1!")
+strength("HelloWorldAndEverythingILove1234!***")
+strength("HelloWorldAndPPL123!!")
+strength("")
+strength("Camtono! ")
 strength("HelloWorld1!")
 strength("H")
